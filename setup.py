0cc2e17fd6e96b54d93406a3c8fdead7a662190e
Anochima <chimaobinice@gmail.com>
2022-03-11 13:57:20 +0100

    Simpoo Ml Project Structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='At Simpoo Business, we strive to build mAI driven software product that help business owners manage multiple ventures at same time with easee. Our AI powered product will be implemented to specifically help classify, validate and improve product images uploaded on our ecommerce platform SimpooCart etc.',
+    author='Ac Nice',
+    license='MIT',
+)
